be8294f0d57e6fdcd4bb975dafa16e3b2a555b8f
KabanovNV <121193540+KabanovNV@users.noreply.github.com>
2023-03-03 17:22:48 +0300

    feat: Верстка 100%

---
+++ js/edit.js
@@ -0,0 +1,47 @@
+let container = document.querySelector('.container');
+
+let editButton = container.querySelector('.profile__edit-button');
+
+let closeButton = container.querySelector('.popup__close-icon')
+
+let formElement = container.querySelector('.popup');
+
+let nameInput = document.getElementById('name');
+
+let jobInput = document.getElementById('about');
+
+
+function popupOpen() {
+
+    formElement.classList.add('popup__opened');
+
+    nameInput.value = container.querySelector('.profile__user-name').textContent;
+
+    jobInput.value = container.querySelector('.profile__user-caption').textContent;
+
+}
+
+
+function popupClose() {
+
+    formElement.classList.remove('popup__opened');
+
+}
+
+function handleFormSubmit(evt) {
+    
+    evt.preventDefault();
+
+    container.querySelector('.profile__user-name').textContent = nameInput.value;
+
+    container.querySelector('.profile__user-caption').textContent = jobInput.value;
+
+    popupClose()
+
+}
+
+editButton.addEventListener('click', popupOpen);
+
+closeButton.addEventListener('click', popupClose);
+
+formElement.addEventListener('submit', handleFormSubmit);
